a1f7588711e7e1e6db94d45702ada61729152dda
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2021-10-07 08:28:57 -0400

    update

--- react-movies/src/components/Home.js
+++ react-movies/src/components/Home.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 //Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 //Components
-
+import HeroImage from "./HeroImage";
 //Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
 //Image
@@ -13,7 +13,20 @@ const Home = () => {
 
     console.log(state);
 
-    return <div>Home Page</div>
+    return (
+        //Fragment as parent element instead of div
+        //If there is a first result in state then render/return the HeroImage with the specified props (using the state) or return null if no first result in state
+        <>
+        {state.results[0] ?
+        <HeroImage 
+            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+            title={state.results[0].original_title}
+            text={state.results[0].overview}
+            /> 
+        : null
+        }
+        </>
+    )
 }
 
 export default Home;
