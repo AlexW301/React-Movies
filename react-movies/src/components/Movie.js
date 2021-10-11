2928d6f5edcde1f986710d862b9b3c9bd1fbed4b
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2021-10-11 08:32:55 -0400

    update

--- react-movies/src/components/Movie.js
+++ react-movies/src/components/Movie.js
@@ -1,14 +1,38 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 //Config
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 //Components
 import Grid from "./Grid";
 import Spinner from "./Spinner";
+import BreadCrumb from "./BreadCrumb";
+import MovieInfo from "./MovieInfo";
+import MovieInfoBar from "./MovieInfoBar";
 //Hook
-
+import { useMovieFetch } from "../hooks/useMovieFetch";
 //Image
 import NoImage from '../images/no_image.jpg';
 
-const Movie = () => <div>Movie</div>
+const Movie = () => {
+    // Below: Name "movieId" on line 18 in App.js
+    const { movieId } = useParams();
+
+    // Below: Destructoring the useMovieFetch hook to be used here
+    //Calling the useFetchMovie hook for the specific movie id, movie/state is now equal to the param movieId
+    const { state: movie, loading, error } = useMovieFetch(movieId);
+
+    console.log(movie)
+
+    if(loading) return <Spinner/>
+    if(error) return <div>Something went wrong...</div>
+
+    return (
+        <>
+        <BreadCrumb movieTitle={movie.original_title}/>
+        <MovieInfo movie={movie} />
+        <MovieInfoBar time={movie.runtime} budget={movie.budget} revenue={movie.revenue} />
+        </>
+    )
+}
 
 export default Movie;
---
+++ react-movies/src/components/MovieInfoBar/MovieInfoBar.styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    min-height: 100px;
+    background: var(--darkGrey);
+    padding: 0 20px;
+`;
+
+export const Content = styled.div`
+    display: flex;
+    max-width: var(--maxWidth);
+    width: 100%;
+    margin: 0 auto;
+
+    .column {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: var(--medGrey);
+        border-radius: 20px;
+    }
+`;
