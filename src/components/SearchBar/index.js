db6b2d5e36968f6b77175589bbd29065ef7870d6
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2021-10-13 08:25:27 -0400

    Update index.js

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,3 +1,51 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+//Image
+import searchIcon from '../../images/search-icon.svg';
+//Styles
+import { Wrapper, Content } from './SearchBar.styles';
+
+class SearchBar extends Component {
+    
+
+    useEffect(() => {
+        // Below: This skips the initial render so this does not run on the first page load
+        if(initial.current) {
+            initial.current = false;
+            return;
+        }
+
+        const timer = setTimeout(() => {
+            setSearchTerm(state)
+        }, 500)
+
+        // Below: Doesnt trigger until the function has finished and is going to run again. This resets the timer so we don't have multiple timers running
+        return () => clearTimeout(timer)
+    }, [setSearchTerm, state])
+
+    return (
+        <Wrapper>
+            <Content>
+                <img src={searchIcon} alt='search-icon' />
+                <input 
+                type='text'
+                placeholder='Search Movie'
+                onChange={event => setState(event.currentTarget.value)}
+                value={state}
+                />
+            </Content>
+        </Wrapper>
+    )
+};
+
+SearchBar.propTypes = {
+    callback: PropTypes.func
+}
+
+export default SearchBar;
+
+/*
+//////// FUNCTIONAL COMPONENT /////////////
 import React, { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 //Image
@@ -45,3 +93,4 @@ SearchBar.propTypes = {
 }
 
 export default SearchBar;
+*/
